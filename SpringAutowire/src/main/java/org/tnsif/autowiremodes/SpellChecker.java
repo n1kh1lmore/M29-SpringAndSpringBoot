4af761105dba050127806bdf9094a252eb1aa2bd
n1kh1lmore <nikupcm10@gmail.com>
2023-07-02 21:24:24 +0530

    First Commit

---
+++ SpringAutowire/src/main/java/org/tnsif/autowiremodes/SpellChecker.java
@@ -0,0 +1,16 @@
+package org.tnsif.autowiremodes;
+
+public class SpellChecker {
+	
+	//DEFAULT CONSTRUCTOR
+	public SpellChecker()
+	{
+		System.out.println("SpellChecker constructor");
+	}
+	
+	public void checkSpeelling()
+	{
+		System.out.println("Checking the spelling and correcting it");
+	}
+
+}
